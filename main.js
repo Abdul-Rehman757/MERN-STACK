1d9275c5f29010df769264619f3581331d01caa1
ML_project <a.rehman360916@gmail.com>
2024-07-18 20:17:56 +0500

    these veriables tasks are done at 11-07-2024

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-<<<<<<< HEAD
-console.log("hello");
-=======
-console.log("hello1");
->>>>>>> feature2
+// console.log("hello");
+// console.log("hello1");
 
-console.log("this is new feature");
+// console.log("this is new feature");
 
-console.log("this is my 2nd feature");
+// console.log("this is my 2nd feature");
+
+// this code refers to veriables
+
+// this is for let veriable
+let a=10;
+console.log(a);
+{
+    let a=15;
+    console.log(a);
+}
+console.log(a);
+
+// this is for const veriable 
+
+const b=20;
+console.log(b);
+{
+    const b=25;
+    console.log(b);
+
+}
+console.log(b);
+
+// this is for var veriable
+
+var c;
+c=30;
+console.log(c);
+{
+    c=40;
+    console.log(c);
+
+}
+console.log(c);
